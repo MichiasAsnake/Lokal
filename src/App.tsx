524423a9dfcd2f3d12ccdf64b2cfc309e06ed8c9
Michias Asnake <mickyasnake@live.com>
2023-04-08 13:18:14 -0700

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from './components/Nav'
 import Nearyou from './components/Nearyou'
+import Categories from './components/Categories'
 
 const App = () => {
   return (
     <div>
+      <Categories />
       <Nav />
       <Nearyou />
     </div>
---
+++ src/components/Categories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div className="cat_container">
+        
+        <div className="cat_box">
+        <div className="cat_pic">
+            
+        </div>
+        <div className="cat_txt">
+            <p>Automotive</p>
+        </div>
+        </div>
+
+    </div>
+  );
+};
+
+export default Categories;
